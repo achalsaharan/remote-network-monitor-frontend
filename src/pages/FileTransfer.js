97afa8c004a5efa7d395f1e5ec695a0e82a7ec23
achal saharan <asaharan_be17@thapar.edu>
2020-12-21 21:43:19 +0530

    improving layout of file transfer page

--- src/pages/FileTransfer.js
+++ src/pages/FileTransfer.js
@@ -1,29 +1,58 @@
 import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import { Container } from '@material-ui/core';
-import { SentimentSatisfied } from '@material-ui/icons';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import FormControl from '@material-ui/core/FormControl';
+import Container from '@material-ui/core/Container';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import SendIcon from '@material-ui/icons/Send';
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
 
 const useStyles = makeStyles((theme) => ({
-    container: {
-        width: '100%',
-        marginTop: theme.spacing(11),
-        
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
     },
-    root: {
-        width: '100%',
-        maxWidth: 360,
-        backgroundColor: '#ddd',
-        margin: '0 auto',
-        alignItems: 'center'
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
     },
-    
+    form: {
+        width: '100%',
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+    formControl: {
+        marginTop: theme.spacing(1),
+        width: '100%',
+    },
 }));
 
-const FileTransfer = () =>{
+export default function SignIn() {
     const classes = useStyles();
+
     const [files, setFiles] = useState([]);
 
     const res = [
@@ -38,23 +67,61 @@ const FileTransfer = () =>{
  
     console.log(files)
 
-    return(
-        <Container className = {classes.container}>
-            <div className = {classes.root}>
-                <TextField id="standard-basic" label="Standard" />
-                {/* <input type="text" placeholder="select file to transfer"></input> */}
-                <List component="nav" aria-label="secondary mailbox folders">
-                    {files.map((item, idx) => {
-                        return (
-                            <React.Fragment key = {idx}>
-                                <ListItem>{item.filename}</ListItem>
-                            </React.Fragment>
-                        );
-                    })}
-                </List>
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <SendIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    File Transfer
+                </Typography>
+                <form className={classes.form} noValidate>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="ip_range"
+                        label="client id"
+                        name="ip_range"
+                        autoFocus
+                    />
+
+                    <FormControl
+                        variant="outlined"
+                        className={classes.formControl}
+                    >
+                        <InputLabel id="demo-simple-select-outlined-label">
+                            file to transfer
+                        </InputLabel>
+                        <Select
+                            labelId="demo-simple-select-outlined-label"
+                            id="demo-simple-select-outlined"
+                            // value={age}
+                            // onChange={handleChange}
+                            label="file to transfer"
+                        >
+                            {files.map((item, idx) => {
+                                return (
+                                    <MenuItem key={idx} value={item.id}>{item.filename}</MenuItem>
+                                )
+                            })}
+                        </Select>
+                    </FormControl>
+
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Transfer
+                    </Button>
+                </form>
             </div>
         </Container>
-    )
+    );
 }
-
-export default FileTransfer
